4d9d3debb6378acebbdee6fd4e0e245b5ef7f0e9
Andrew Graziano <andrew.graziano@icloud.com>
2024-10-28 18:33:18 -0500

    updated  code

--- project1Code.py
+++ project1Code.py
@@ -1,43 +1,43 @@
-from pybricks.pupdevices import Motor, UltrasonicSensor
+from pybricks.pupdevices import Motor
+from pybricks.hubs import PrimeHub
 from pybricks.parameters import Port, Stop, Direction
 from pybricks.tools import wait, StopWatch
+from pybricks.robotics import DriveBase
 
-
-MAX_OBSTACLES = 25  # Maximum number of obstacles
-num_obstacles = 13  # Number of obstacles
-
-# Obstacle locations (coordinates of their centers)
-obstacles = [
-    [0.61, 2.743], [0.915, 2.743], [1.219, 2.743], [1.829, 2.743],
-    [1.829, 1.524], [1.829, 1.829], [1.829, 2.134], [2.743, 0.305],
-    [2.743, 0.61], [2.743, 0.915], [2.743, 2.743], [3.048, 2.743],
-    [3.353, 2.743], [-1, -1], [-1, -1], [-1, -1], [-1, -1],
-    [-1, -1], [-1, -1], [-1, -1], [-1, -1], [-1, -1], [-1, -1],
-    [-1, -1], [-1, -1]
-]
-
-# Start and goal locations
-start = [0.305, 1.219]
-goal = [3.658, 1.829]
-
-
-
-rear_motor = Motor(Port.A, Direction.CLOCKWISE)
-front_motor = Motor(Port.E, Direction.COUNTERCLOCKWISE)
+hub = PrimeHub()
+left = Motor(Port.E, Direction.CLOCKWISE)
+right = Motor(Port.F, Direction.COUNTERCLOCKWISE)
 watch = StopWatch() 
 
-rear_motor.control.limits(acceleration=1000)
-front_motor.control.limits(acceleration=1000)
+driver = DriveBase(left, right, wheel_diameter=56, axle_track=112)
 
-def drive(rear_motor_speed, front_motor_speed):
-    rear_motor.run(rear_motor_speed)
-    front_motor.run(front_motor_speed)
 
+hub.display.number(69)
+hub.imu.reset_heading(0)
+
+
+left.control.limits(acceleration=1000)
+right.control.limits(acceleration=1000)
+
+#make it drive only 1 meter forward
+def drive():
+    drive_base
+
+#make it to turn a perfect 90 
+def turn(target_angle, speed):
+    current_angle = hub.imu.heading()
+    direction = 1 if target_angle < current_angle else -1
+
+    while abs(hub.imu.heading() - target_angle) > 1:  # 1 degree tolerance
+        left.run(direction * speed)
+        right.run(-direction * speed)
+    
+    left.stop()
+    right.stop()
+    hub.imu.reset_heading(0)
+
+    
 watch.reset()
-while watch.time() < 5000:
-    drive(0, 300)
-    wait(3000)
-    drive(300, 0)
-    wait(3000)
-    drive(300, 300)
-    wait(3000)
+# while watch.time() < 3000:
+driver.straight(348)
+turn(90, 69)
